0a4d2b36a74a247b7d67c5a4f1a91f8f31d8c0fe
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-01-09 23:10:49 +0530

    Create Question 12.cpp

---
+++ Amazon/Question 12.cpp
@@ -0,0 +1,37 @@
+//Column name from a given column number 
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string str;
+        while(n > 0)
+        {
+            char res='A'+(n-1)%26;
+            str=res+str;
+        n=(n-1)/26;
+        }
+        return str;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends
