a3369a81d5c039027a80551f860657e6f68426da
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-01-11 23:21:45 +0530

    Create Question 3.cpp

---
+++ Microsoft/Question 3.cpp
@@ -0,0 +1,60 @@
+//Rotate by 90 degree 
+
+
+// { Driver Code Starts
+//Initial Template for C
+
+
+
+#include<stdio.h>
+
+ // } Driver Code Ends
+//User function Template for C
+
+
+void rotate(int n,int a[][n])
+{
+    //code here
+    
+        for(int i = n/2 + n%2; i < n; i++)
+        {
+            for(int j = n/2; j < n; j++)
+            {
+                int temp = a[i][j];
+                a[i][j] = a[j][n-1-i];
+                a[j][n-1-i] = a[n-1-i][n-1-j];
+                a[n-1-i][n-1-j] = a[n-1-j][i];
+                a[n-1-j][i] = temp;
+            }
+        }
+    
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d",&t); 
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n][n];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+                scanf("%d",&a[i][j]);
+        }
+        rotate(n,a);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                printf("%d ",a[i][j]);
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
