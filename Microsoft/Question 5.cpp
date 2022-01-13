bcad43dcb9d0850513ba5c690618c26aabc730c4
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-01-13 19:17:05 +0530

    Create Question 5.cpp

---
+++ Microsoft/Question 5.cpp
@@ -0,0 +1,84 @@
+// Stock span problem
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       vector<int>vec;
+       stack<pair<int,int>>st;
+       for(int i=0;i<n;i++){
+           if(st.size()==0){
+               vec.push_back(-1);
+           }
+           
+           else if(st.size()>0 && st.top().first>price[i]){
+               vec.push_back(st.top().second);
+           }
+           
+           else if(st.size()>0 && st.top().first <= price[i]){
+               while(st.size()>0 && st.top().first <= price[i]){
+                   st.pop();
+               }
+               if(st.size()==0){
+                   vec.push_back(-1);
+               }
+               else{
+                   vec.push_back(st.top().second);
+               }
+           }
+       
+           st.push({price[i],i});
+       }
+           for(int i = 0; i < vec.size(); i++){
+               vec[i] = i - vec[i];
+           }
+           return vec;
+        
+        
+           
+           
+       }
+    
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
