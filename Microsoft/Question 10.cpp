12b9fdf601d3eef66bab8b01ff229e2050e2fae5
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-01-14 22:11:29 +0530

    Create Question 10.cpp

---
+++ Microsoft/Question 10.cpp
@@ -0,0 +1,71 @@
+//Stickler Thief 
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        
+        if(n==0)
+        {  
+            return 0;
+        }
+        
+        else if(n==1)
+        { 
+             return arr[0];
+        }
+        
+        else if(n==2)
+        { 
+            return max(arr[0],arr[1]);
+        }
+        
+
+         int res = 0;
+         int val1 = arr[0];
+         int val2 = max(arr[0],arr[1]);
+         for(int i = 2 ; i < n ; i++){
+            
+            res = max(arr[i]+val1,val2);
+            val1 = val2;
+            val2 = res;
+            
+    }
+    return res;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
